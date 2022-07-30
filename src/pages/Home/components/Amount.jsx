3eebf8b714ccdc9ce635b962d9276a8611b0e733
Luan Bitar <7@lbitar.com>
2022-07-30 18:05:54 -0300

    feat: adding inputMode

--- src/pages/Home/components/Amount.jsx
+++ src/pages/Home/components/Amount.jsx
@@ -39,7 +39,13 @@ function Amount({ amount, setAmount, period, setPeriod }) {
         What is your total income?
       </p>
       <div className="mt-2 flex shadow-md rounded-full">
-        <Input symbol="$" value={amount} type="tel" onChange={onAmountChange} />
+        <Input
+          symbol="$"
+          value={amount}
+          type="tel"
+          inputMode="numeric"
+          onChange={onAmountChange}
+        />
         <Select options={periodOptions} value={period} onChange={setPeriod} />
       </div>
     </div>
