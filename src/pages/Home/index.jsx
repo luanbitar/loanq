5c193da6fe48d22e63387b3f5b3f2cf586340323
Luan Bitar <7@lbitar.com>
2022-07-31 00:29:33 -0300

    feat:  adding spacing bottom in home to mobile

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,23 +10,25 @@ function Home() {
   const [incomeType, setIncomeType] = React.useState("GROSS");
 
   return (
-    <div className="bg-white min-w-[100vw] min-h-[100vh] flex justify-center items-center flex-col md:flex-row relative overflow-hidden">
-      <div className="absolute top-[-85px] w-[800px] md:top-[-320px] left-1/2 transform translate-x-[-50%] md:w-[2000px] m-auto">
-        <HomeHeaderIcon />
-      </div>
-      <div className="flex sm:pr-5 mt-10 md:mt-0 mb-5 md:mb-[283px] max-w-[320px] md:max-w-[470px] max-h-[350px] z-10">
-        <HomeIllustration />
-      </div>
-      <div className="max-w-[400px] flex-1 z-10">
-        <Amount
-          amount={amount}
-          setAmount={setAmount}
-          period={period}
-          setPeriod={setPeriod}
-        />
-        <div className="flex px-4">
-          <Radio value={incomeType} onChange={setIncomeType} />
-          <CalcButton state={{ amount, period, incomeType }} />
+    <div className="overflow-x-hidden">
+      <div className="bg-white min-w-[100vw] min-h-[calc(100vh+200px)] sm:min-h-[100vh] flex justify-center items-center flex-col md:flex-row relative">
+        <div className="absolute top-[-85px] w-[800px] md:top-[-320px] left-1/2 transform translate-x-[-50%] md:w-[2000px] m-auto">
+          <HomeHeaderIcon />
+        </div>
+        <div className="flex sm:pr-5 mt-[197px] md:mt-0 mb-5 md:mb-[283px] max-w-[320px] md:max-w-[470px] max-h-[350px] z-10">
+          <HomeIllustration />
+        </div>
+        <div className="max-w-[400px] flex-1 z-10">
+          <Amount
+            amount={amount}
+            setAmount={setAmount}
+            period={period}
+            setPeriod={setPeriod}
+          />
+          <div className="flex px-4">
+            <Radio value={incomeType} onChange={setIncomeType} />
+            <CalcButton state={{ amount, period, incomeType }} />
+          </div>
         </div>
       </div>
     </div>
